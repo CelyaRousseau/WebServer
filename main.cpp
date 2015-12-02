686ead4995efee8a2e955aa4d8c490fd34e5984c
Lethgir <lethgir@gmail.com>
2015-12-02 16:11:25 +0100

    demande type serv OK

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>           // std::cout
 #include <stdio.h>
 
+using namespace std;
 
 int main( int argc, const char* argv[] )
 {
-	if(argc == 1) {
-		string type = argv[0];
-		printf("Vous avez demandé le type de serveur : %s", type);
+	if(argc == 2) {
+		std::string type = argv[1];
+		cout << "Vous avez demandé le type de serveur : " << type.c_str() << endl;
 
 
-	} else printf("Indiquer le type de serveur désiré !");
+	} else cout << "Indiquer le type de serveur désiré !" << endl;
 }
