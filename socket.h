40517326c4ddac2a345127a4987c01434ce5ed16
Lethgir <lethgir@gmail.com>
2016-03-11 11:28:26 +0100

    create socket class

--- socket.h
+++ socket.h
@@ -1,7 +1,25 @@
+#include <stdio.h>
+#include <string.h> 
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <fstream>
+#include <iostream>
+#include <unistd.h>
+
 class socket {
 	private:
-		int attribut1;
+		// Variables
+		int socket_infos;
+		int socket_client;
+		int c;
+		int *socket_new;
+		struct sockaddr_in server, client;
 
 	public:
-		int maMethode(int i);
+		socket();
+		void listen();
+		int accept();
+		int getSocketClient();
+		void *socket::socket_handler(void *socket_infos);
 };
