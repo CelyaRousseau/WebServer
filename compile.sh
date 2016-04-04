bc53241d1b363611d9ff44924b3a14b980a92613
CelyaRousseau <celyarousseau.pro@gmail.com>
2016-04-04 14:08:58 +0200

    fix(compile script) : script point on old server instead of real server we want to use

--- compile.sh
+++ compile.sh
@@ -3,16 +3,16 @@
 if [ "$1" != "" ]; then
 	case "$1" in
 		"epoll") 	echo "Compiling EPOLL server"
-					g++ server_epoll.cpp -std=c++11 -g -pthread -o server -Wno-write-strings 
+					g++ servers/Epoll.cpp -std=c++11 -g -pthread -o server -Wno-write-strings 
 					;;
 		"fork") 	echo "Compiling FORK server"
-					g++ server_fork.cpp -std=c++11 -g -pthread -o server -Wno-write-strings 
+					g++ servers/Fork.cpp -std=c++11 -g -pthread -o server -Wno-write-strings 
 					;; 
 		"thread") 	echo "Compiling THREAD server"
-					g++ server_thread.cpp -std=c++11 -g -pthread -o server -Wno-write-strings
+					g++ servers/Thread.cpp -std=c++11 -g -pthread -o server -Wno-write-strings
 					;;
 		"threadp") 	echo "Compiling THREAD POOL server"
-					g++ server_threadpool.cpp -std=c++11 -g -pthread -o server -Wno-write-strings 
+					g++ servers/ThreadP.cpp -std=c++11 -g -pthread -o server -Wno-write-strings 
 					;;
 		*) echo "usage ${0} epoll:fork:thread:threadp"
 					;;
